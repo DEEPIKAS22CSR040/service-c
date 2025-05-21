e35a0f37a12423b32d90e282ba3d158844b6d3b0
Deepika S <deepikas.22cse@kongu.edu>
2025-05-21 15:03:09 +0530

    Update app.py

--- service-c/app.py
+++ service-c/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Service C!"
+    return jsonify({"message": "Hello from service-c!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
