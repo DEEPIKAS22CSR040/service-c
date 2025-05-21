4d0fd16a72bbe3ba1ce666b9f759671f0e72e0fc
Deepika S <deepikas.22cse@kongu.edu>
2025-05-21 15:03:52 +0530

    Update test_app.py

--- service-c/test_app.py
+++ service-c/test_app.py
@@ -1,4 +1,16 @@
-import app
+import unittest
+from app import app
 
-def test_home():
-    assert app.home() == "Hello from Service C!"
+class BasicTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Hello from service-b!", response.data)
+
+if __name__ == '__main__':
+    unittest.main()
